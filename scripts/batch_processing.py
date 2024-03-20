9c65cc14af97130e623fc8aeab3c5833d4e18b82
YMtheory <932899789@qq.com>
2024-03-19 20:28:23 -0700

    Rename batch_processing class.

--- scripts/batch_processing.py
+++ scripts/batch_processing.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scripts.CryoAsicAnalysis import CryoAsicAnalysis
 
 
-class comparison:
+class batch:
     def __init__(self) -> None:
         self.analysers          = {}
         self.nevent_dict        = {}
@@ -54,19 +54,25 @@ class comparison:
         self.noncoupled_channelId = np.array([ i for i in range(64) if i not in chids])
 
     ## noise analysis
-    def calculate_noise(self):
+    def calculate_noise(self, calcpsd=False, subtract_baseline=False):
         for lb, ana in self.analysers.items():
-            #ana.calculate_avg_psds() # We must calculate PSD before stds in this script...
+            if subtract_baseline:
+                ana.baseline_subtract()
+            if calcpsd:
+                ana.calculate_avg_psds() # We must calculate PSD before stds in this script...
             ana.calculate_stds()
             self.noise_dfs[lb] = ana.noise_df
 
 
     ## plotter
     
-    def _plot_oneChannel_oneEvent(self, evtid, chid):
+    def _plot_waveforms_oneChannel_oneEvent(self, lbs, evtid, chid):
         
         fig, ax = plt.subplots(figsize=(12, 8))
-        for lb, ana in self.analysers.items():
+        if len(lbs) == 0:
+            lbs = self.analysers.keys()
+        for lb in lbs:
+            ana = self.analysers[lb]
             time = ana.times
             ax.plot(time, self._get_waveform_oneChannel_oneEvent(ana, evtid, chid), label=lb)
         
@@ -79,9 +85,11 @@ class comparison:
         return fig    
 
 
-    def _plot_std(self):
+    def _plot_std(self, lbs):
         fig, ax = plt.subplots(figsize=(12, 8))
-        for lb, _ in self.analysers.items():
+        if len(lbs) == 0:
+            lbs = self.analysers.keys()
+        for lb in lbs:
             stds = self._get_std_oneSet(lb)
             ax.plot(stds, 'o-', label=lb)
         ax.set_xlabel('Channel ID', fontsize=13)
@@ -93,10 +101,12 @@ class comparison:
         return fig    
 
 
-    def _plot_std_separately(self):
+    def _plot_std_separately(self, lbs):
         fig, ax = plt.subplots(1, 2, figsize=(12, 6))
         print(self.noncoupled_channelId)
-        for lb, _ in self.analysers.items():
+        if len(lbs) == 0:
+            lbs = self.analysers.keys()
+        for lb in lbs:
             stds = self._get_std_oneSet(lb)
             ax[0].plot(self.coupled_channelId, stds[self.coupled_channelId], 'o-', label=lb)
             ax[1].plot(self.noncoupled_channelId, stds[self.noncoupled_channelId], 'o-', label=lb)
@@ -111,4 +121,24 @@ class comparison:
         return fig    
 
 
+    def _plot_waveforms_oneEvent(self, lbs, channels, evtid):
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+        
+        if len(lbs) == 0:
+            lbs = self.analysers.keys()
+        for lb in lbs:
+            ana = self.analysers[lb]
+            for cha in channels:
+                time = ana.times
+                one_wave = self._get_waveform_oneChannel_oneEvent(ana, evtid, cha)
+                ax.plot(time, one_wave, label=f'cha {cha}')
+                
+        ax.set_xlabel('time [us]', fontsize=13)
+        ax.set_ylabel('ADC', fontsize=13)
+        ax.tick_params(axis='both', which='major', labelsize=13)
+        ax.legend(fontsize=13)
+        
+        plt.tight_layout()
+        return fig
 
